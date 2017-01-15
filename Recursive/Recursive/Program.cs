9847e3d2db68314fe71e1e5373c92e32650fca9f
William Ward <wward246@gmail.com>
2017-01-14 18:53:58 -0800

    New Commit

---
+++ Recursive/Recursive/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Recursive f = new Recursive();
+            int n,fact_1, sum,sumInt,k,x,y,a,b, fibo;
+
+            Console.Write("Enter a number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            fact_1 = f.fact(n);
+            sum = f.sum(n);
+            sumInt = f.sum_int(123459874);
+
+            Console.Write("Enter a number to countdown from: ");
+            k = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a base number: ");
+            x= Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a power number: ");
+            y= Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a number for A: ");
+            a= Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a number for B: ");
+            b= Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a number for Fibonacci Series: ");
+            fibo = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.WriteLine("\nThe factorial of " + n + " is: " + fact_1);
+            Console.WriteLine("\nThe Sum of " + n + " is: " + sum);
+            Console.WriteLine("\nThe Sum of " + n + " is: " + sumInt);
+            Console.Write("\n\nCountdown from: ");
+            f.count(k);
+            Console.Write("\n\nCounting to: ");
+            f.countRev(k);
+
+            Console.Write("\nBinary number of "+k+" is: ");
+            f.binary(k);
+
+            Console.Write("\n" + k + " to the power of" +" is: " + f.power(x, y));
+
+            Console.Write("\n\nThe greatest common denumerator is: " + f.GCD(a, b));
+            Console.Write("\n\nFibonacci number sequence of " +fibo+" is: " );
+
+            for(int start=0; start<=fibo; start++)
+            {
+                Console.Write(f.fib(start) + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Recursive/Recursive/Recursive.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive
+{
+    public class Recursive 
+    {
+        int n { get; set; }
+
+     
+
+        public int fact(int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * fact(n - 1); 
+
+        }
+
+        public int sum(int n)
+        {
+            if (n == 0)
+                return 0;
+            else
+                return n + sum(n - 1);
+        }
+
+        public int sum_int(int n)
+        {
+
+            if (n == 0)
+                return 0;
+            else
+                return n % 10 + sum_int(n / 10);
+
+        }
+        public void count(int n)
+        {
+           
+            if (n == 0)
+                return;
+            else
+                Console.Write( + n + ", ");
+                count(n - 1); 
+                
+        }
+        public void countRev(int n)
+        {
+
+            if (n == 0)
+                return;
+            else
+                countRev(n - 1);
+                Console.Write(+n + ", ");
+        }
+        public void binary(int n)
+        {
+            if (n == 0)
+                return;
+            else
+                binary(n / 2);
+            Console.Write(n % 2 + "");
+         }
+        
+        public float power(int x, int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return x * power(x, n - 1);
+        }
+
+        public int GCD(int a, int b)
+        {
+            if (b == 0)
+                return a;
+            else
+                return GCD(b, a % b);
+        }
+
+        public int fib(int n)
+        {
+            if (n==0)
+                return 0;
+            if (n == 1)
+                return 1;
+            else
+                return fib(n - 1) + fib(n - 2);
+        }
+
+
+
+    }
+}
